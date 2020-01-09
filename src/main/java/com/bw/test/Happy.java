7923552b5a274f96c8efa07349889f1d0493985e
huge_pro <hugeGit@hg.com>
2020-01-09 11:12:40 +0800

    add test left

---
+++ src/main/java/com/bw/test/Happy.java
@@ -0,0 +1,16 @@
+/**
+ * Create Date:2020年1月9日
+ */
+package com.bw.test;
+
+/**
+ * <br>Title:TODO 类标题
+ * <br>Description:TODO 类功能描述
+ * <br>Author:胡正林(823865618@qq.com)
+ * <br>Date:2020年1月9日
+ */
+public class Happy {
+  public static void main(String[] args) {
+    System.out.println("right...");
+  }
+}
