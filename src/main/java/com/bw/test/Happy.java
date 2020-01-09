eb065d9fdd8c61b05641229a865dbb7323e0260b
huge_pro <hugeGit@hg.com>
2020-01-09 14:20:20 +0800

    edit on branch hot_fix

--- src/main/java/com/bw/test/Happy.java
+++ src/main/java/com/bw/test/Happy.java
@@ -13,5 +13,6 @@ public class Happy {
   public static void main(String[] args) {
     System.out.println("left.....");
     System.out.println("right...");
+    System.out.println("hot fix ....");
   }
 }
